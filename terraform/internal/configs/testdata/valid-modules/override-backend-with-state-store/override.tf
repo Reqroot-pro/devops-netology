12203cdddc8fefdd860a428987326a92b0c0514d
Дамир Гайнуллин <reqruit.pro@yandex.ru>
2025-07-21 18:40:24 +0300

    new edit

--- terraform/internal/configs/testdata/valid-modules/override-backend-with-state-store/override.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  // Note: not valid config - a paired entry in required_providers is usually needed
-  state_store "foo_override" {
-    provider "foo" {}
-
-    custom_attr = "override"
-  }
-}
