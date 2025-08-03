cf448edacc45501eaa5458162929109548c9f798
Дамир Гайнуллин <reqruit.pro@yandex.ru>
2025-08-03 15:15:08 +0300

    new

--- terraform/02/src/main.tf
+++ terraform/02/src/main.tf
@@ -10,14 +10,14 @@ resource "yandex_vpc_subnet" "develop" {
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = var.vm_web_image_family
+  family = "ubuntu-2004-lts"
 }
 resource "yandex_compute_instance" "platform" {
-  name        = var.vm_web_name
-  platform_id = var.vm_web_platform_id
+  name        = "netology-develop-platform-web"
+  platform_id = "standart-v4"
   resources {
-    cores         = 2
-    memory        = 2
+    cores         = 1
+    memory        = 1
     core_fraction = 5
   }
   boot_disk {
@@ -39,46 +39,3 @@ resource "yandex_compute_instance" "platform" {
   }
 
 }
-**"netology-develop-platform-db"**
-
-resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
-}
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
-}
-
-data "yandex_compute_image" "ubuntu" {
-  family = var.vm_web_image_family
-}
-resource "yandex_compute_instance" "platform" {
-  name        = var.vm_web_name
-  platform_id = var.vm_web_platform_id
-  resources {
-    cores         = 2
-    memory        = 2
-    core_fraction = 5
-  }
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.image_id
-    }
-  }
-  scheduling_policy {
-    preemptible = true
-  }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
-  }
-
-  metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
-  }
-
-}
-
