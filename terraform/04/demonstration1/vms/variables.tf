bcc5fd7534c224fce5b1d97d018c8bc177d719af
Damir Gainullin <reqroot.pro@gmail.com>
2025-11-13 16:28:38 +0300

    new

--- terraform/04/demonstration1/vms/variables.tf
+++ terraform/04/demonstration1/vms/variables.tf
@@ -27,3 +27,29 @@ variable "default_zone" {
   type    = string
   default = "ru-central1-a"
 }
+
+# IP-адрес (строка)
+variable "ip_address" {
+  type        = string
+  description = "IP-адрес"
+  default     = "192.168.0.1"
+
+  validation {
+    condition     = can(cidrhost("${var.ip_address}/32", 0))
+    error_message = "Переменная ip_address должна быть корректным IP-адресом, например 192.168.0.1"
+  }
+}
+
+# Список IP-адресов
+variable "ip_list" {
+  type        = list(string)
+  description = "Список IP-адресов"
+  default     = ["192.168.0.1", "1.1.1.1", "127.0.0.1"]
+
+  validation {
+    condition = alltrue([
+      for ip in var.ip_list : can(cidrhost("${ip}/32", 0))
+    ])
+    error_message = "Все элементы ip_list должны быть корректными IP-адресами"
+  }
+}
